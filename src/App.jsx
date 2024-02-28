c420d15a451c21f4a6d5f28aa46163ab80cb3565
Shubh <shubh2268@gmail.com>
2024-02-28 17:33:56 +0530

    --

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
-import './App.css';
 
 function App() {
 
   return (
-    <div>
-      Hello Shubh
+    <div className='app'>
+
+      <div className='overlay'>
+        <div className='container'>
+
+          <div className='section section__inputs'>
+            <input type='text' name='city' placeholder='Enter City...' />
+            <button>°F</button>
+          </div>
+
+          <div className='section section__temprature'>
+            <div className='icon'>
+              <h3>{`Delhi, IN`}</h3>
+              <img src='' alt='weatherIcon' />
+              <h3>Haze</h3>
+            </div>
+
+            <div className='temprature'>
+              <h1>24°C</h1>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
     </div>
   )
 }
