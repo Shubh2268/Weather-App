17f959470924ec532bec8545ab5ed8cd214fc3bc
Shubh <shubh2268@gmail.com>
2024-02-29 18:06:36 +0530

    --

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,65 +1,68 @@
 import React from 'react';
 import './description.css';
-import { FaArrowUp, FaArrowDown, FaWind } from "react-icons/fa";
-import { BiHappy } from "react-icons/bi";
-import { MdCompress, MdOutlineWaterDrop } from "react-icons/md";
+import { FaArrowUp, FaArrowDown, FaWind } from 'react-icons/fa';
+import { BiHappy } from 'react-icons/bi';
+import { MdCompress, MdOutlineWaterDrop } from 'react-icons/md';
 
-const Description = () => {
+const Description = ({ weather, units }) => {
+
+    const tempUnit = units === 'metric' ? '°C' : '°F';
+    const windUnit = units === 'metric' ? 'm/s' : 'm/h';
 
     const cards = [
         {
             id: 1,
             icon: <FaArrowDown />,
-            title: "min",
-            data: '20',
-            unit: '℃'
+            title: 'min',
+            data: weather.temp_min.toFixed(),
+            unit: tempUnit
         }, {
             id: 2,
             icon: <FaArrowUp />,
-            title: "max",
-            data: '20',
-            unit: ''
+            title: 'max',
+            data: weather.temp_max.toFixed(),
+            unit: tempUnit,
         },
         {
             id: 3,
             icon: <BiHappy />,
-            title: "feels like",
-            data: '19',
-            unit: '℃'
+            title: 'feels like',
+            data: weather.feels_like.toFixed(),
+            unit: tempUnit,
         },
         {
             id: 4,
             icon: <MdCompress />,
-            title: "pressure",
-            data: '1013',
-            unit: "hPa",
+            title: 'pressure',
+            data: weather.pressure,
+            unit: 'hPa',
         },
         {
             id: 5,
             icon: <MdOutlineWaterDrop />,
-            title: "humidity",
-            data: '24%',
-            unit: "%",
+            title: 'humidity',
+            data: weather.humidity,
+            unit: '%',
         },
         {
             id: 6,
             icon: <FaWind />,
-            title: "wind speed",
-            data: '3',
-            unit: 'm/s',
-        }
-    ];
+            title: 'wind speed',
+            data: weather.speed.toFixed(),
+            unit: windUnit,
+        },
+    ]
 
     return (
-        <div className="section section__descriptions">
+        <div className='section section__descriptions'>
             {cards.map(({ id, icon, title, data, unit }) => (
 
-                <div key={id} className="card">
-                    <div className="description__card-icon">
+                <div key={id} className='card'>
+                    <div className='description__card-icon'>
                         {icon}
                         <small>{title}</small>
                     </div>
-                    <h2>{`20 C`}</h2>
+                    <h2>{`${data} ${unit}`}</h2>
                 </div>
 
             ))}
